a71388941e4b761810172a94e58ffece96489719
iakovleva <paulina.iakovleva@gmail.com>
2018-03-13 10:29:10 +0200

    New method get_stat_goals

---
+++ 4_get_stat_goals.py
@@ -0,0 +1,28 @@
+from tokens import *
+
+import json
+
+from urllib.request import urlopen
+
+#адрес для отправки json-запросов
+# url = 'https://api.direct.yandex.ru/v4/json/'
+url = 'https://api-sandbox.direct.yandex.ru/v4/json/'
+
+data = {
+   'method': 'GetStatGoals',
+   'param':{
+       'CampaignID': [258015],
+        },
+   'token': token,
+   'locale': 'ru',
+    }
+
+#конвертировать словарь в JSON-формат и перекодировать в UTF-8
+jdata = json.dumps(data, ensure_ascii=False).encode('utf8')
+
+#выполнить запрос
+#response = urllib2.urlopen(url,jdata)
+
+response = urlopen(url,jdata)
+#вывести результат
+print (response.read().decode('utf8'))
